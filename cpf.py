73e1d7995f4621386978dbb6c9bb8e6d10374949
Rafael M <magalhaesrafael07@gmail.com>
2022-09-02 15:30:44 -0300

    Altera os métodos valida_cpf e formata_cpf utilizando a biblioteca validate_docbr.

--- cpf.py
+++ cpf.py
@@ -1,26 +1,23 @@
+from validate_docbr import CPF
+
 class Cpf:
     def __init__(self, cpf):
         cpf = str(cpf)
-        if self.valida_tamanho(cpf):
+        if self.valida_cpf(cpf):
             self.cpf = cpf
         else:
             raise ValueError("CPF é inválido!")
 
-    def valida_tamanho(self, cpf):
+    def valida_cpf(self, cpf):
         if len(cpf) == 11:
-            return True
+            valido = CPF()
+            return valido.validate(cpf)
         else:
-            return False
+            raise ValueError('Quantidade de dígitos é inválida.')
 
     def formata_cpf(self):
-        return (
-            '{}.{}.{}-{}'.format(
-                self.cpf[:3],
-                self.cpf[3:6],
-                self.cpf[6:9],
-                self.cpf[9:]
-            )
-        )
+        mascara = CPF()
+        return mascara.mask(self.cpf)
 
     def __str__(self):
         return self.formata_cpf()
